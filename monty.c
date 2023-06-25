bdb3842bc0d58068d61284ab5f5e8adf78104ac7
James Ngandu <jamesmngandu@gmail.com>
2023-06-25 23:05:38 +0300

    added

---
+++ monty.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
+/**
+* main - a monty code interpreter
+* @argc: number of arguments
+* @argv: an array - monty file location
+* Return: on success - 0
+*/
+int main(int argc, char *argv[])
+{
+	char *content;
+	FILE *file;
+	size_t len = 0;
+	ssize_t readLine = 1;
+	stack_t *stack = NULL;
+	unsigned int count = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (readLine > 0)
+	{
+		content = NULL;
+		readLine = getline(&content, &len, file);
+		bus.content = content;
+		count++;
+		if (readLine > 0)
+		{
+			_exec(content, &stack, count, file);
+		}
+		free(content);
+	}
+	/frees stack/
+	freeStack(stack);
+	fclose(file);
+return (0);
+}
