392213ce9509d9e6f5319bcea9dbe9874d0c6b65
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-25 13:52:44 -0700

    sub

---
+++ sub.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+  *_sub-  subtracts the top element from the second top element of the stack
+  *@head: stack head
+  *@count: line count
+  *Return: void
+ */
+void _sub(stack_t **head, unsigned int count)
+{
+	stack_t *ax;
+	int val, n;
+
+	ax = *head;
+	for (n = 0; ax != NULL; n++)
+		ax = ax->next;
+	if (n < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", count);
+		fclose(bus.file);
+		free(bus.line_content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	ax = *head;
+	val = ax->next->n - ax->n;
+	ax->next->n = val;
+	*head = ax->next;
+	free(ax);
+}
