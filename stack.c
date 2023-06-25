2d92b04cb51a4d30fb012d846a3493052b6db893
RONALD MUTUA <ronaldmutua617@gmail.com>
2023-06-25 13:48:01 -0700

    stack

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * _stack - sets the format of the data to a stack (LIFO
+ * @head: stack head
+ * @counter: line count
+ * Return: void - no return
+*/
+void _stack(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	bus.lifi = 0;
+}
