4aac58f8fbd3b157361d48a73313e8c001872bc2
James Ngandu <jamesmngandu@gmail.com>
2023-06-25 23:26:35 +0300

    added

---
+++ pchar.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * _pchar - prints the char at the top of the stack, followed by a new line
+ * @head: head of the stack
+ * @count: line count
+ * Return: void
+*/
+void _pchar(stack_t **head, unsigned int count)
+{
+	stack_t *tmp;
+
+	tmp = *head;
+	if (tmp == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", count);
+		fclose(bus.file);
+		free(bus.line_content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (tmp->n > 127 || tmp->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(bus.file);
+		free(bus.line_content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", tmp->n);
+}
